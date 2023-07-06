73b1afdaa2b30cd8c0f6897a2b70aae10cd13358
sofialazrak <sofia.lazrak@gmail.com>
2023-07-06 15:51:18 +0100

    Filter

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
+const clearBtn = document.getElementById('clear')
+const filter = document.getElementById('filter')
 
 const addItem = (e) => {
     e.preventDefault();
@@ -21,6 +23,8 @@ const addItem = (e) => {
 
     itemList.appendChild(li)
 
+    checkUI()
+
     itemInput.value = ''
 }
 
@@ -38,5 +42,51 @@ const createIcon = (classes) => {
     return icon
 }
 
+const removeItem = (e) => {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        if (confirm('Are you sure ?')) {
+            e.target.parentElement.parentElement.remove()
+            checkUI()
+        }
+    }
+}
+
+const clearItems = () => {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild)
+    }
+    checkUI()
+}
+
+const filterItems = (e) => {
+    const text = e.target.value.toLowerCase()
+    const items = document.querySelectorAll('li')
+    items.forEach(item => {
+        const itemName = item.firstChild.textContent.toLowerCase()
+        if (itemName.indexOf(text) === -1) {
+            item.style.display = 'none'
+        } else {
+            item.style.display = 'flex'
+        }
+    })
+}
+
+
+const checkUI = () => {
+    const items = itemList.querySelectorAll('li')
+    if (items.length === 0) {
+        clearBtn.style.display = 'none'
+        filter.style.display = 'none'
+    } else {
+        clearBtn.style.display = 'block'
+        filter.style.display = 'block'
+    }
+}
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem)
+itemList.addEventListener('click', removeItem)
+clearBtn.addEventListener('click', clearItems)
+filter.addEventListener('input', filterItems)
+
+checkUI()
